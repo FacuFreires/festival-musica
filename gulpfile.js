17f7b66d84febd16f7eb2e371680a5847a06c05c
Facundo Freires <facufreires1@gmail.com>
2022-07-29 17:16:54 -0300

    subo archivos modificados

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
+const { src, dest, watch } = require("gulp");  // src pára identificar y dest para almacenar
+const sass = require("gulp-sass")(require("sass"));   // traigo sass
 
-function tarea(cb) {
-    console.log('mi primer tarea');
+function css(done) {
+    // 1ro identificar el archivo de SASS
+    // 2do Compilarlo
+    // 3ro Almacenarla en el disco duro
+    src('src/scss/app.scss')  // identifica
+    .pipe(sass())  // compila 
+    .pipe(dest("build/css")); // donde se almacena
 
-    cb();  // Pasamos una callback para que no nos tire algun error
-};
+    done(); // callback que avisa a gulp cuando llegamos al final de la ejecución de la función
+}
 
-exports.tarea = tarea;  // Cuando mande a llamar primerTarea, se va a ejecutar tarea()
+function dev(done) {
+    watch("src/scss/app.scss", css);  // Cuando cambie algo en el archivo, se va a llamar la funcion css
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
